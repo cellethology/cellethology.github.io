4357df4585648c99f5ad1ba63e8f94d96b2ae17b
neonine2 <jerry.wang95@yahoo.ca>
2025-03-09 22:04:44 +0800

    switched to webp

--- js/content.js
+++ js/content.js
@@ -28,25 +28,25 @@ const siteContent = {
         {
             title: "Building Foundation Models to Characterize Cellular Interactions via Geometric Self-Supervised Learning on Spatial Genomics",
             description: "A foundation model for cell interactions",
-            image: "/asset/pub/CIFM.png",
+            image: "/asset/pub/CIFM.webp",
             url: "https://www.biorxiv.org/content/10.1101/2025.01.25.634867v1"
         },
         {
             title: "Identifying perturbations that boost T-cell infiltration into tumors via counterfactual learning of their spatial proteomic profiles",
             description: "Find drug targets from tumor images",
-            image: "/asset/pub/tumor.png",
+            image: "/asset/pub/tumor.webp",
             url: "https://www.nature.com/articles/s41551-025-01357-0"
         },
         {
             title: "Localization of signaling receptors maximizes cellular information acquisition in spatially structured natural environments",
             description: "How cells navigate complex environments",
-            image: "/asset/pub/cell_sense.png",
+            image: "/asset/pub/cell_sense.webp",
             url: "https://www.cell.com/cell-systems/fulltext/S2405-4712(22)00225-3"
         },
         {
             title: "Inferring gene regulation dynamics from static snapshots of gene expression variability",
             description: "Learn dynamical systems from non-dynamical data",
-            image: "/asset/pub/clock.png",
+            image: "/asset/pub/clock.webp",
             url: "https://journals.aps.org/pre/abstract/10.1103/PhysRevE.104.044406"
         }
     ],
@@ -57,49 +57,49 @@ const siteContent = {
             title: "A cellular solution to a robotics problem",
             authors: "Zitong Jerry Wang, Matt Thomson",
             journal: "BioRxiv (2025)",
-            image: "/asset/pub/robotic.png",
+            image: "/asset/pub/robotic.webp",
             url: "https://www.biorxiv.org/content/10.1101/2025.02.12.637580v1"
         },
         {
             title: "Building Foundation Models to Characterize Cellular Interactions via Geometric Self-Supervised Learning on Spatial Genomics",
             authors: "Yuning You, Zitong Jerry Wang, Kevin Fleisher, Rex Liu, Matt Thomson",
             journal: "BioRxiv (2025)",
-            image: "/asset/pub/CIFM.png",
+            image: "/asset/pub/CIFM.webp",
             url: "https://www.biorxiv.org/content/10.1101/2025.01.25.634867v1"
         },
         {
             title: "Identifying perturbations that boost T-cell infiltration into tumors via counterfactual learning of their spatial proteomic profiles",
             authors: "Zitong Jerry Wang, Abdullah Farooq, Yu-Jen Chen, Aman Bhargava, Alexander Xu, Matt Thomson",
             journal: "Nature Biomedical Engineering (2025)",
-            image: "/asset/pub/tumor.png",
+            image: "/asset/pub/tumor.webp",
             url: "https://www.nature.com/articles/s41551-025-01357-0"
         },
         {
             title: "ViTally Consistent: Scaling Biological Representation Learning for Cell Microscopy",
             authors: "Kian Kenyon-Dean, Zitong Jerry Wang, John Urbanik, Konstantin Donhauser, Jason Hartford, Saber Saberian, Nil Sahin, Ihab Bendidi, Safiye Celik, Marta Fay, Juan Sebastián Rodríguez Vera, Imran S Haque, Oren Kraus ",
             journal: "ICLR FM4Science Runner-up Best Paper (2024)",
-            image: "/asset/pub/cells.png",
+            image: "/asset/pub/cells.webp",
             url: "https://openreview.net/forum?id=niywLsa54R"
         },
         {
             title: "Multi-ContrastiveVAE disentangles perturbation effects in single cell images from optical pooled screens",
             authors: "Zitong Jerry Wang, Romain Lopez, Jan-Christian Hütter, Takamasa Kudo, Heming Yao, Philipp Hanslovsky, Burkhard Höckendorf, Rahul Moran, David Richmond, Aviv Regev",
             journal: "ICLR 2024 Workshop on Machine Learning for Genomics Explorations",
-            image: "/asset/pub/cell.png",
+            image: "/asset/pub/cell.webp",
             url: "https://openreview.net/pdf?id=PcpeaehdlL"
         },
         {
             title: "Localization of signaling receptors maximizes cellular information acquisition in spatially structured natural environments",
             authors: "Zitong Jerry Wang, Matt Thomson",
             journal: "Cell Systems (2022)",
-            image: "/asset/pub/cell_sense.png",
+            image: "/asset/pub/cell_sense.webp",
             url: "https://www.cell.com/cell-systems/fulltext/S2405-4712(22)00225-3"
         },
         {
             title: "Inferring gene regulation dynamics from static snapshots of gene expression variability",
             authors: "Euan Joly-Smith, Zitong Jerry Wang, Andreas Hilfinger",
             journal: "Physical Review E (2021)",
-            image: "/asset/pub/clock.png",
+            image: "/asset/pub/clock.webp",
             url: "https://journals.aps.org/pre/abstract/10.1103/PhysRevE.104.044406"
         }
     ],
@@ -108,22 +108,22 @@ const siteContent = {
     teamMembers: [
         {
             name: "Jerry Wang",
-            image: "/asset/team/jerry_wang.jpg",
+            image: "/asset/team/jerry_wang.webp",
             title: "Principal Investigator"
         },
         {
             name: "Shiyu Jiang",
-            image: "/asset/team/shiyu_jiang.jpg",
+            image: "/asset/team/shiyu_jiang.webp",
             title: "Visiting Student"
         },
         {
             name: "Lanning Liu",
-            image: "/asset/team/lanning_liu.jpg",
+            image: "/asset/team/lanning_liu.webp",
             title: "Administrative Assistant"
         },
         {
             name: "Xuyin Liu",
-            image: "/asset/team/xuyin_liu.jpg",
+            image: "/asset/team/xuyin_liu.webp",
             title: "Research Assistant"
         },
     ]
